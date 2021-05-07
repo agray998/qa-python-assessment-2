8ee5aff8efd5f1180d85602d8ec2d68eb8b6d877
Adam Gray <a.gray.4797@gmail.com>
2021-05-07 10:07:36 +0100

    question 2 complete

--- questions/python2.py
+++ questions/python2.py
@@ -58,7 +58,12 @@ def one(string):
 
 
 def two(number):
-    return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+        else:
+            pass
+    return True
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------
 
